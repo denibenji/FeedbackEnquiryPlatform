f02f7b8ea1a8fd9526b3418c51eb1ef847d0c6f3
Ben Chan <cs.chan.2011@sis.smu.edu.sg>
2014-04-28 12:52:59 +0800

    Initial Commit

---
+++ PublicFeedback/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PublicFeedback
+//
+//  Created by Ben Chan on 28/4/14.
+//  Copyright (c) 2014 IS480_FYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ PublicFeedback/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PublicFeedback
+//
+//  Created by Ben Chan on 28/4/14.
+//  Copyright (c) 2014 IS480_FYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
